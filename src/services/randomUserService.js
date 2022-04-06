f87d6f2f3a7091ce9fe3cc7c86e60d8468dcdae6
Micael Santos <micael.pereira1999@hotmail.com>
2022-04-05 23:05:31 -0300

    chore: user data persistence

--- src/services/randomUserService.js
+++ src/services/randomUserService.js
@@ -4,5 +4,16 @@ export const randomUserService = {
   getAll: async () => {
     const { data } = await axios.get('https://randomuser.me/api?results=7'); 
     return data;
+  },
+
+  save: (user) => localStorage.setItem('colab.user', JSON.stringify(user)),
+
+  getUser: async () => {
+    const userStorage = localStorage.getItem('colab.user');
+    
+    if (userStorage) return JSON.parse(userStorage);
+
+    const { data } = await axios.get('https://randomuser.me/api');
+    return data.results[0];
   }
 }
